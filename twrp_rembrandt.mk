2290efd54dd4a54d068907b2ca64e0e12b2ee763
莫名其妙2333 <50542889+mominqimiang@users.noreply.github.com>
2024-03-10 17:03:11 +0800

    Update twrp_rembrandt.mk

--- twrp_rembrandt.mk
+++ twrp_rembrandt.mk
@@ -7,6 +7,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Configure compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Configure virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
@@ -20,8 +26,10 @@ $(call inherit-product, device/xiaomi/rembrandt/device.mk)
 PRODUCT_DEVICE := rembrandt
 PRODUCT_NAME := twrp_rembrandt
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 22122RK93C
+PRODUCT_MODEL := Redmi K60E
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Hide Reflash TWRP
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable=true
